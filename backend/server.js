6c127c3c98d5d0714617b8ce7d18ac6527e15757
hlb-git <mzeelovegeneral@gmail.com>
2025-01-20 18:02:29 +0100

    feat: created helper funtion to parse csv file uploads

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,12 @@
 const express = require('express');
+const multer = require('multer');
 const mysql = require('mysql2');
 const fs = require('fs');
-
 const csv = require('csv-parser');
 const bodyParser = require('body-parser');
 
 
+const PORT = 5050;
 const app = express();
 app.use(bodyParser.json());
 
@@ -19,4 +20,17 @@ const db = mysql.createConnection({
 db.connect((err) => {
   if (err) throw err;
   console.log("Connection to database is successful");
+});
+
+
+app.post('upload', (req, res) => {
+  
+
+
+
+// app.get('/metrics', (req, res) => 
+
+
+app.listen(PORT, () => {
+  console.log(`Server running on port: ${PORT}...`);
 });
