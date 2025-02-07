30c4842c6d52b02325d2a96f1821d185b97c702e
hlb-git <mzeelovegeneral@gmail.com>
2025-02-07 23:04:03 +0100

    feat: added landing page to the application

--- frontend/src/pages/landingpage.tsx
+++ frontend/src/pages/landingpage.tsx
@@ -1,5 +1,5 @@
 import './landingpage.css';
-import illustration from '../assets/landingimage.jpg';
+import illustration from '../assets/landingimage.png';
 import {Link} from 'react-router-dom';
 
 
@@ -11,15 +11,32 @@ export function Landing() {
       <div className="h-wrapper">
         <div className="header">
           <span className="logo">
-            InsightFlair
+            Flair
           </span>
           <div className="links">
             <Link to="#"> Overview </Link>  
             <Link to="#"> FAQ </Link>
+            <Link to="#"> Privacy and terms </Link>
+            <div className="login">
             <Link to="/auth"> Login </Link> 
+            </div>
           </div>
           <div>
           </div>
+        </div>
+      </div>
+      <div className="hero-container">
+        <div className="hero-texts">
+          <h1 className="primary"> Welcome to InsightFlair</h1>
+          <h2 className="secondry"> Find the Story Behind Your Numbers </h2>
+          <h3 className="detail"> Upload your CSV, let our system do the rest—store, analyze, and infere Insight.</h3> 
+          <div className="buttons">
+            <button className="get-started"> Get Started </button>
+            <button className="learn-more"> Learn More </button>
+          </div>
+        </div>
+        <div className="hero-image">
+          <img src={illustration}/>
         </div>
       </div>
       </div>
